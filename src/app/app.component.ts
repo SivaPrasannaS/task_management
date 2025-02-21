0799ce8ac549e0e3e3f0abe57dfa6d3f04f08492
SivaPrasannaS <sivaprasannas2001@gmail.com>
2025-02-21 21:35:08 +0530

    Initial commit 'Task Management' scaffolding and implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { UserComponent } from "./components/user/user.component";
+import { TaskComponent } from "./components/task/task.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
